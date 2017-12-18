ba590567e6d7527f1a7effd3c406616440649f6e
JLuo <jeffreyzcluo@gmail.com>
2017-12-18 21:14:25 +0800

    refactored code to code_generator.py

---
+++ code_generator.py
@@ -0,0 +1,137 @@
+import re
+import json
+from my_lib import get_list_of_dicts, get_dict_from_string
+
+def generate_image_code(command):
+    code = ""
+    indent = 1
+
+    code += "\t"*indent + "session.send(new builder.Message(session)\n"
+    indent += 1
+    code += "\t"*indent + ".attachments([\n"
+    indent += 1
+    #all the urls seperated by commas
+    urls = re.search("{.*}", command).group()[1:-1].split(',')
+
+    #dst.write("\t"*indent + "{\n" + "\t"*(indent + 1) + "contentUrl : '" + ("',\n" + "\t"*(indent + 1) + "contentType : 'image/jpeg'\n" + "\t"*indent + "},\n" + "\t"*indent + "{\n" + "\t"*(indent + 1) + "contentUrl : '").join(urls) + "',\n" + "\t"*(indent + 1) + "contentType : 'image/jpeg'\n" + "\t"*indent + "}\n")
+
+    for i in range(len(urls)):
+        #get all parameters here
+        url = urls[i]
+        code += "\t"*indent + "{\n" + "\t"*(indent + 1) + "contentUrl : '" + url + "',\n"
+        code += "\t"*(indent + 1) + "contentType : 'image/jpeg'\n" + "\t"*indent + "},\n"
+
+    code = code[:-2] + "\n"
+
+    indent -= 1
+    code += "\t"*indent + "])\n"
+    indent -= 1
+    code += "\t"*indent + ");\n"
+
+    return code
+
+def generate_text_code(command):
+    code = ""
+    indent = 1
+    #all the texts seperated by commas
+    texts = re.search("{.*}", command).group()[1:-1].split(',')
+
+    for i in range(len(texts)):
+        #get all parameters here
+        text = texts[i]
+        code += "\t"*indent + "session.send(`" + text + "`);\n"
+
+    return code
+
+
+def generate_herocard_code(command):
+    code = ""
+    indent = 1
+
+    """
+    for herocards, if there contains buttons that are for user selection,
+    they need to be put in a prompt.choice and not send
+    """
+    herocards = get_list_of_dicts(re.search("{.*}", command).group())
+
+    code += "\t"*indent + "let herocards = new builder.Message(session)\n"
+    indent += 1
+    #if there is more than one argument, then put them in a carousel
+    if len(herocards) > 1:
+        code += "\t"*indent + ".attachmentLayout(builder.AttachmentLayout.carousel)\n"
+    code += "\t"*indent + ".attachments([\n"
+    indent += 1
+    #all the herocards
+
+    for i in range(len(herocards)):
+        #herocard is a python dictionary with fields matched with their values
+        herocard = herocards[i]
+        code += "\t"*indent + "new builder.HeroCard(session)\n"
+        indent += 1
+        #build card with fields in herocard
+
+        #see if each field is defined in the dictionary
+        if "text" in herocard:
+            code += "\t"*indent + ".text(" + herocard["text"] + ")"
+        if "images" in herocard:
+            code += "\n" + "\t"*indent + ".images([\n"
+            indent += 1
+            #use string join to concatenate the image builders
+            code += "\t"*indent + 'builder.CardImage.create(session, "' + (",\n" + "\t"*indent + 'builder.CardImage.create(session, "').join([x + '")' for x in herocard["images"]])
+
+            indent -= 1
+            code += "\n" + "\t"*indent + "])"
+
+        if "buttons" in herocard and "links" in herocard:
+            code += "\n" + "\t"*indent + ".buttons([\n"
+            indent += 1
+            #use string join to concatenate the button builders
+            #see which one appeared in the original command first
+            if command.find("buttons") < command.find("links"):
+                code += "\t"*indent + 'builder.CardAction.postBack(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.postBack(session, "').join([x + '", "' + x + '")' for x in herocard["buttons"]]) + ",\n"
+                code += "\t"*indent + 'builder.CardAction.openUrl(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.openUrl(session, "').join([x[1] + '", "' + x[0] + '")' for x in herocard["links"]])
+            else:
+                code += "\t"*indent + 'builder.CardAction.openUrl(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.openUrl(session, "').join([x[1] + '", "' + x[0] + '")' for x in herocard["links"]]) + ",\n"
+                code += "\t"*indent + 'builder.CardAction.postBack(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.postBack(session, "').join([x + '", "' + x + '")' for x in herocard["buttons"]])
+
+            indent -= 1
+            code += "\n" + "\t"*indent + "])"
+        elif "buttons" in herocard:
+            code += "\n" + "\t"*indent + ".buttons([\n"
+            indent += 1
+            #use string join to concatenate the button builders
+            code += "\t"*indent + 'builder.CardAction.postBack(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.postBack(session, "').join([x + '", "' + x + '")' for x in herocard["buttons"]])
+
+            indent -= 1
+            code += "\n" + "\t"*indent + "])"
+
+        elif "links" in herocard:
+            code += "\n" + "\t"*indent + ".buttons([\n"
+            indent += 1
+            #use string join to concatenate the button builders
+            code += "\t"*indent + 'builder.CardAction.openUrl(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.openUrl(session, "').join([x[1] + '", "' + x[0] + '")' for x in herocard["links"]])
+
+            indent -= 1
+            code += "\n" + "\t"*indent + "])"
+
+        indent -= 1
+        if i < len(herocards) - 1:
+            code += ",\n"
+        else:
+            code += "\n"
+
+    indent -= 1
+    code += "\t"*indent + "])\n"
+    indent -= 1
+    code += "\t"*indent + ");\n"
+
+    #if this herocard contains buttons user could click on
+    #use prompt.choice
+    if any("buttons" in herocard for herocard in herocards):
+        for herocard in herocards:
+            if "buttons" in herocard:
+                code += "\t"*indent + "builder.Prompts.choice(session, herocards, " + json.dumps(herocard["buttons"]) + ");"
+    else:
+        code += "\t"*indent + "session.send(herocards);\n"
+
+    return code
--- installation_steps_generator.py
+++ installation_steps_generator.py
@@ -12,6 +12,11 @@ import sys
 import re
 import json
 from my_lib import get_list_of_dicts, get_dict_from_string
+from code_generator import(
+    generate_text_code,
+    generate_image_code,
+    generate_herocard_code
+    )
 
 source_file_name = sys.argv[1]
 destination_file_name = source_file_name.split('.')[0] + ".js"
@@ -33,124 +38,17 @@ with open(source_file_name, "r") as src, open(destination_file_name, "w") as dst
 
             action = command.split()[0]
             if action == "image":
-                dst.write("\t"*indent + "session.send(new builder.Message(session)\n")
-                indent += 1
-                dst.write("\t"*indent + ".attachments([\n")
-                indent += 1
-                #all the urls seperated by commas
-                urls = re.search("{.*}", command).group()[1:-1].split(',')
-
-                #dst.write("\t"*indent + "{\n" + "\t"*(indent + 1) + "contentUrl : '" + ("',\n" + "\t"*(indent + 1) + "contentType : 'image/jpeg'\n" + "\t"*indent + "},\n" + "\t"*indent + "{\n" + "\t"*(indent + 1) + "contentUrl : '").join(urls) + "',\n" + "\t"*(indent + 1) + "contentType : 'image/jpeg'\n" + "\t"*indent + "}\n")
-                
-                for i in range(len(urls)):
-                    #get all parameters here
-                    url = urls[i]
-                    dst.write("\t"*indent + "{\n" + "\t"*(indent + 1) + "contentUrl : '" + url + "',\n")
-                    if i < len(urls) - 1:
-                        dst.write("\t"*(indent + 1) + "contentType : 'image/jpeg'\n" + "\t"*indent + "},\n")
-                    else:
-                        dst.write("\t"*(indent + 1) + "contentType : 'image/jpeg'\n" + "\t"*indent + "}\n")
-                
-                indent -= 1
-                dst.write("\t"*indent + "])\n")
-                indent -= 1
-                dst.write("\t"*indent + ");\n")
+                #fix the indentation by replacing first tab with correct number of tabs
+                dst.write(re.sub(r"^\t|(?<=[^\t])\t", "\t"*indent, generate_image_code(command)))
     
             elif action == "text":
-                #all the texts seperated by commas
-                texts = re.search("{.*}", command).group()[1:-1].split(',')
-
-                for i in range(len(texts)):
-                    #get all parameters here
-                    text = texts[i]
-                    dst.write("\t"*indent + "session.send(`" + text + "`);\n")
+                #fix the indentation by replacing first tab with correct number of tabs
+                dst.write(re.sub(r"^\t|(?<=[^\t])\t", "\t"*indent, generate_text_code(command)))
                 
             elif action == "herocard":
-                """
-                for herocards, if there contains buttons that are for user selection,
-                they need to be put in a prompt.choice and not send
-                """
-                herocards = get_list_of_dicts(re.search("{.*}", command).group())
+                #fix the indentation by replacing first tab with correct number of tabs
+                dst.write(re.sub(r"^\t|(?<=[^\t])\t", "\t"*indent, generate_herocard_code(command)))
                 
-                dst.write("\t"*indent + "let herocards = new builder.Message(session)\n")
-                indent += 1
-                #if there is more than one argument, then put them in a carousel
-                if len(herocards) > 1:
-                    dst.write("\t"*indent + ".attachmentLayout(builder.AttachmentLayout.carousel)\n")
-                dst.write("\t"*indent + ".attachments([\n")
-                indent += 1
-                #all the herocards
-
-                for i in range(len(herocards)):
-                    #herocard is a python dictionary with fields matched with their values
-                    herocard = herocards[i]
-                    dst.write("\t"*indent + "new builder.HeroCard(session)\n")
-                    indent += 1
-                    #build card with fields in herocard
-                    
-                    #see if each field is defined in the dictionary
-                    if "text" in herocard:
-                        dst.write("\t"*indent + ".text(" + herocard["text"] + ")")
-                    if "images" in herocard:
-                        dst.write("\n" + "\t"*indent + ".images([\n")
-                        indent += 1
-                        #use string join to concatenate the image builders
-                        dst.write("\t"*indent + 'builder.CardImage.create(session, "' + (",\n" + "\t"*indent + 'builder.CardImage.create(session, "').join([x + '")' for x in herocard["images"]]))
-                        
-                        indent -= 1
-                        dst.write("\n" + "\t"*indent + "])")
-                        
-                    if "buttons" in herocard and "links" in herocard:
-                        dst.write("\n" + "\t"*indent + ".buttons([\n")
-                        indent += 1
-                        #use string join to concatenate the button builders
-                        #see which one appeared in the original command first
-                        if command.find("buttons") < command.find("links"):
-                            dst.write("\t"*indent + 'builder.CardAction.postBack(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.postBack(session, "').join([x + '", "' + x + '")' for x in herocard["buttons"]]) + ",\n")
-                            dst.write("\t"*indent + 'builder.CardAction.openUrl(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.openUrl(session, "').join([x[1] + '", "' + x[0] + '")' for x in herocard["links"]]))
-                        else:
-                            dst.write("\t"*indent + 'builder.CardAction.openUrl(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.openUrl(session, "').join([x[1] + '", "' + x[0] + '")' for x in herocard["links"]]) + ",\n")
-                            dst.write("\t"*indent + 'builder.CardAction.postBack(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.postBack(session, "').join([x + '", "' + x + '")' for x in herocard["buttons"]]))
-                            
-                        indent -= 1
-                        dst.write("\n" + "\t"*indent + "])")
-                    elif "buttons" in herocard:
-                        dst.write("\n" + "\t"*indent + ".buttons([\n")
-                        indent += 1
-                        #use string join to concatenate the button builders
-                        dst.write("\t"*indent + 'builder.CardAction.postBack(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.postBack(session, "').join([x + '", "' + x + '")' for x in herocard["buttons"]]))
-                        
-                        indent -= 1
-                        dst.write("\n" + "\t"*indent + "])")
-                        
-                    elif "links" in herocard:
-                        dst.write("\n" + "\t"*indent + ".buttons([\n")
-                        indent += 1
-                        #use string join to concatenate the button builders
-                        dst.write("\t"*indent + 'builder.CardAction.openUrl(session, "' + (",\n" + "\t"*indent + 'builder.CardAction.openUrl(session, "').join([x[1] + '", "' + x[0] + '")' for x in herocard["links"]]))
-                        
-                        indent -= 1
-                        dst.write("\n" + "\t"*indent + "])")
-                        
-                    indent -= 1
-                    if i < len(herocards) - 1:
-                        dst.write(",\n")
-                    else:
-                        dst.write("\n")
-                    
-                indent -= 1
-                dst.write("\t"*indent + "])\n")
-                indent -= 1
-                dst.write("\t"*indent + ");\n")
-                            
-                #if this herocard contains buttons user could click on
-                #use prompt.choice
-                if any("buttons" in herocard for herocard in herocards):
-                    for herocard in herocards:
-                        if "buttons" in herocard:
-                            dst.write("\t"*indent + "builder.Prompts.choice(session, herocards, " + json.dumps(herocard["buttons"]) + ");")
-                else:
-                    dst.write("\t"*indent + "session.send(herocards);\n")
                     
             elif action == "thumbnailcard":
                 """
